cc32825ef2a2c83858674aed45991451590581b5
kininavami <60567021+kininavami@users.noreply.github.com>
2020-10-24 09:27:57 +0530

    Commtting from tdf directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import {User} from './user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = ['angular', 'react','none'];
+  userModel = new User('','9mikini@gmail.com',1234558799,'angular','evening_5PM',true);
 }
---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: number,
+        public course: string,
+        public timePref: string,
+        public subscribe: boolean
+    ){}
+
+}
